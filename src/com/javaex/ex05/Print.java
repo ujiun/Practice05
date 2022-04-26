43b4912c3055ccce37ee3ab538b1699755e4f7f0
ujiun <ujiun@hanmail.net>
2022-04-26 09:32:37 +0900

    1

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -7,5 +7,13 @@ public class Print {
     }
 
     //메소드  3개 를 작성하세요
-    
+    public void printer(boolean val){
+        System.out.println(val);
+    }
+    public void printer(double val){
+        System.out.println(val);
+    }
+    public void printer(String val){
+        System.out.println(val);
+    }
 }
