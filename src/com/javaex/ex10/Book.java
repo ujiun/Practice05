e28ae28bc85ca4eceb764ebdac3983947fdb46df
ujiun <ujiun@hanmail.net>
2022-05-08 18:13:41 +0900

    ing

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,9 +1,50 @@
 package com.javaex.ex10;
 
 public class Book {
+	//필드
+    private int bookNo;
+    private String title;
+    private String author;
+    private int stateCode;
+    
+    //생성자
+    public Book() {
+	}
+    
+    public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCode = 1;
+	}
+    
+    //메소드-gs
+    public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
     
     
     
-    
-    
+    //메소드-일반
+	        
 }
